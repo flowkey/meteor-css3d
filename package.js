c87d191f986df2492b7bb8021603b0c40c0b3377
Geordie J <geojay@gmail.com>
2015-05-26 14:14:38 +0200

    Remove boilerplate comments from package js

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
-  name: 'css3d',
+  name: 'ephemer:css3d',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Thin & performant abstraction for transforming, dragging, and resizing elements using CSS3 matrices',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/ephemer/meteor-css3d.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
